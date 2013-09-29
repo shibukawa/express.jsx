8d531eaf369365a21fc2d12443b262c59e9cffc3
Yoshiki Shibukawa <yshibukawa@ngmoco.com>
2013-09-29 01:52:45 -0700

    Implement middlewares, add samples/directory-sample.jsx

---
+++ HISTORY.md
@@ -0,0 +1,18 @@
+History
+==========
+
+0.1.2 (09/28/2013)
+---------------------
+
+* Implement middlewares
+* Add `samples/directory-sample.jsx`
+
+0.1.1 (09/28/2013)
+---------------------
+
+* Fix github URL (tnx @yosuke_furukawa !)
+
+0.1.0 (09/28/2013)
+---------------------
+
+* First working version (except middle ware)
--- samples/directory-sample.jsx
+++ samples/directory-sample.jsx
@@ -1,13 +1,16 @@
 import "console.jsx";
 import "express.jsx";
 
+/**
+ * Run this sample at express.jsx root folder (it contains package.json).
+ */
 class _Main {
     static function main(argv : string[]) : void
     {
         var app = express.create();
-        app.get('/', function (req : Request, res : Response) : void {
-            res.send('hello world');
-        });
+
+        app.use(express.directory('samples'));
+        app.use(express.static_('samples'));
 
         console.log("running at port 3000");
         app.listen(3000);
